709bd8da8d1e15f82fd61e116966060a7cc19912
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-05-01 11:10:24 +0200

    done with array, object, typestring

--- src/index.js
+++ src/index.js
@@ -14,3 +14,37 @@ const petName = 'Kaika';
  */
 
 const number = 1000;
+
+/**
+ *
+ * My Array
+ * @type {Array<number>}
+ *
+ */
+const myArray = [10, 45.12, 132, 100];
+// const myArray=[10,45.12,132,100,'Hi',true]
+
+/**
+ * Pet object
+ * @type{{id:number, name:string,age:number, address:string,gender:string}}
+ */
+const pet = {
+  id: 1,
+  name: 'DogO',
+  age: 2,
+  address: 'Kigali',
+  gender: 'Male',
+};
+
+/**
+ * person object
+ * @type{{name:string, age:number,job:string,address:string,earn:number| string}}
+ */
+
+const person = {
+  name: 'Carla',
+  job: 'Developer',
+  address: 'Kigali',
+  age: 25,
+  earn: '452',
+};
